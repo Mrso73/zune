b186f0b19b295b5b8f297ce31d861e72a3a4ca98
Mrso73 <olinguerreroancher6@gmail.com>
2025-02-14 15:30:41 -0600

    Decoupled the renderer and shader structs

--- src/renderer/renderer.zig
+++ src/renderer/renderer.zig
@@ -6,14 +6,11 @@ const err = @import("../core/gl.zig");
 const Model = @import("model.zig").Model;
 const Mesh = @import("mesh.zig").Mesh;
 const Material = @import("material.zig").Material;
-const Shader = @import("shader.zig").Shader;
 
 const Mat4 = @import("../math/matrix.zig").Mat4;
 
 
 pub const Renderer = struct {
-    color_shader: Shader,
-    textured_shader: Shader,
     allocator: std.mem.Allocator,
 
 
@@ -27,58 +24,9 @@ pub const Renderer = struct {
         // Initialize OpenGL
         c.glEnable(c.GL_DEPTH_TEST);
 
-
-        // Color Shader (no texture)
-        const color_vert = 
-            \\#version 330 core
-            \\layout (location=0) in vec3 aPos;
-            \\uniform mat4 model;
-            \\uniform mat4 view;
-            \\uniform mat4 projection;
-            \\void main() {
-            \\    gl_Position = projection * view * model * vec4(aPos, 1.0);
-            \\}
-        ;
-        const color_frag = 
-            \\#version 330 core
-            \\out vec4 FragColor;
-            \\uniform vec4 color;
-            \\void main() { FragColor = color; }
-        ;
-        var color_shader = try Shader.create(color_vert, color_frag);
-        try color_shader.cacheUniforms(&.{ "model", "view", "projection", "color" });
-
-        // Textured Shader
-        const tex_vert = 
-            \\#version 330 core
-            \\layout (location=0) in vec3 aPos;
-            \\layout (location=1) in vec2 aTexCoord;
-            \\out vec2 TexCoord;
-            \\uniform mat4 model; uniform mat4 view; uniform mat4 projection;
-            \\void main() {
-            \\    gl_Position = projection * view * model * vec4(aPos, 1.0);
-            \\    TexCoord = aTexCoord;
-            \\}
-        ;
-        const tex_frag = 
-            \\#version 330 core
-            \\in vec2 TexCoord;
-            \\out vec4 FragColor;
-            \\uniform vec4 color;
-            \\uniform sampler2D texSampler;
-            \\void main() {
-            \\    FragColor = texture(texSampler, TexCoord) * color;
-            \\}
-        ;
-        var textured_shader = try Shader.create(tex_vert, tex_frag);
-        try textured_shader.cacheUniforms(&.{ "model", "view", "projection", "color", "texSampler" });
-
-
         const render_ptr = try allocator.create(Renderer);
         render_ptr.* = .{
             .allocator = allocator,
-            .color_shader = color_shader,
-            .textured_shader = textured_shader,
         };
         return render_ptr;
     }
@@ -131,6 +79,7 @@ pub const Renderer = struct {
         mesh.draw(); // Draw Mesh
     }
 
+
     pub fn drawModel(self: *Renderer, model: *Model, model_matrix: *const [16]f32, view_matrix: *const [16]f32, projection_matrix: *const [16]f32) !void {
 
         // Iterate over each mesh-material pair
@@ -147,9 +96,6 @@ pub const Renderer = struct {
     // ============================================================
 
     pub fn release(self: *Renderer) void {
-        self.color_shader.deinit();
-        self.textured_shader.deinit();
-
         self.allocator.destroy(self);
     }
 };
--- src/renderer/shader.zig
+++ src/renderer/shader.zig
@@ -72,6 +72,65 @@ pub const Shader = struct {
     }
 
 
+    /// Create a color shader to use
+    pub fn createColorShader() !Shader {
+
+        // Color Shader (no texture)
+        const color_vert = 
+            \\#version 330 core
+            \\layout (location=0) in vec3 aPos;
+            \\uniform mat4 model;
+            \\uniform mat4 view;
+            \\uniform mat4 projection;
+            \\void main() {
+            \\    gl_Position = projection * view * model * vec4(aPos, 1.0);
+            \\}
+        ;
+        const color_frag = 
+            \\#version 330 core
+            \\out vec4 FragColor;
+            \\uniform vec4 color;
+            \\void main() { FragColor = color; }
+        ;
+        var color_shader = try Shader.create(color_vert, color_frag);
+        try color_shader.cacheUniforms(&.{ "model", "view", "projection", "color" });
+
+        return color_shader;
+    }
+
+
+    /// Create texture shader to use
+    pub fn createTextureShader() !Shader {
+
+        // Textured Shader
+        const tex_vert = 
+            \\#version 330 core
+            \\layout (location=0) in vec3 aPos;
+            \\layout (location=1) in vec2 aTexCoord;
+            \\out vec2 TexCoord;
+            \\uniform mat4 model; uniform mat4 view; uniform mat4 projection;
+            \\void main() {
+            \\    gl_Position = projection * view * model * vec4(aPos, 1.0);
+            \\    TexCoord = aTexCoord;
+            \\}
+        ;
+        const tex_frag = 
+            \\#version 330 core
+            \\in vec2 TexCoord;
+            \\out vec4 FragColor;
+            \\uniform vec4 color;
+            \\uniform sampler2D texSampler;
+            \\void main() {
+            \\    FragColor = texture(texSampler, TexCoord) * color;
+            \\}
+        ;
+        var textured_shader = try Shader.create(tex_vert, tex_frag);
+        try textured_shader.cacheUniforms(&.{ "model", "view", "projection", "color", "texSampler" });
+
+        return textured_shader;
+    }
+
+
     // ============================================================
     // Public API: Operational Functions
     // ============================================================
